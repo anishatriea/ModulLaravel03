98e6f1e804f9a17e155607762dddfb09b413a5a2
anishatriea <41880068+anishatriea@users.noreply.github.com>
2023-05-17 14:44:46 +0700

    Add files via upload

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import.meta.glob(["../images/**"]);
+import.meta.glob(["../images/bs-icons.png"]);
+import.meta.glob(["../images/bs-themes.png"]);
+import.meta.glob(["../images/logo-black.svg"]);
+import.meta.glob(["../images/logo-white.svg"]);
+import.meta.glob(["../images/main.svg"]);
